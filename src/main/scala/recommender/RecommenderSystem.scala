5c0fe78f62f1433cdcc1baac0d8b675552319fca
Szymcio <hamletolog@gmail.com>
2019-05-27 23:59:26 +0200

    Make recommender system actor accept message to find similar books

--- src/main/scala/recommender/RecommenderSystem.scala
+++ src/main/scala/recommender/RecommenderSystem.scala
@@ -8,7 +8,8 @@ import org.apache.spark.mllib.recommendation.MatrixFactorizationModel
 
 object RecommenderSystem {
   case object Train {}
-  case class GenerateRecommendations(userId: Int)
+  case class GenerateRecommendationsForUser(userId: Int)
+  case class FindSimilarBooks(bookId: Int)
   case class Recommendation(contentItemId: Int, rating: Double)
   case class Recommendations(items: Seq[Recommendation])
 
@@ -23,22 +24,27 @@ class RecommenderSystem(sc: SparkContext) extends Actor with ActorLogging {
 
   def receive = {
     case Train => trainModel()
-    case GenerateRecommendations(userId) => generateRecommendations(userId, 10)
+    case GenerateRecommendationsForUser(userId) => generateRecommendationsForUser(userId, 10)
+    case FindSimilarBooks(bookId) => findSimilarBooks(bookId, 10)
     case ModelTrainer.TrainingResult(model) => storeModel(model)
   }
 
-  private def trainModel() = {
+  private def trainModel(): Unit = {
     // Start a separate actor to train the recommendation system.
     // This enables the service to continue service requests while it learns new recommendations.
     val trainer = context.actorOf(ModelTrainer.props(sc), "model-trainer")
     trainer ! ModelTrainer.Train
   }
 
-  private def storeModel(model: MatrixFactorizationModel) = {
+  private def findSimilarBooks(bookId: Int, count: Int): Unit = {
+    log.info(s"Finding ${count} books similar to book with ID ${bookId}")
+  }
+
+  private def storeModel(model: MatrixFactorizationModel): Unit = {
     this.model = Some(model)
   }
 
-  private def generateRecommendations(userId: Int, count: Int) = {
+  private def generateRecommendationsForUser(userId: Int, count: Int): Unit = {
     log.info(s"Generating ${count} recommendations for user with ID ${userId}")
 
     // Generate recommendations based on the machine learning model.
